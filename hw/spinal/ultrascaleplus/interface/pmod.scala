4da8c52e8ed4cca1113d8fe86d104962b09ffb2e
denishoornaert <dhoornae@ulb.ac.be>
2025-04-03 18:40:50 +0200

    hardware: ultrascale: pmod: add TCL and XDC generation

--- hw/spinal/ultrascaleplus/interface/pmod.scala
+++ hw/spinal/ultrascaleplus/interface/pmod.scala
@@ -1,29 +1,47 @@
 package ultrascaleplus.interface.pmod
 
+
 import spinal.core._
 import spinal.lib._
 
-import generic.interface.pmod._
-import ultrascaleplus.scripts._
+
+import ultrascaleplus.utils._
 
 
-abstract class AbstractPMOD() {
+object PMOD {
+  
+  def apply(pins: Seq[String]): PMOD = {
+    return new PMOD(pins)
+  }
 
-  var port: Option[PMOD] = None
+}
 
-  val names: Array[String]
+
+class PMOD(pins: Seq[String]) extends Vec[Bool](Bool, Vector.fill(pins.length)(Bool())) with PSPLInterface {
+
+  assert(
+    assertion = (pins.length <= 8),
+    message   = f"Length given (${pins.length}) is larger than the allowed length of 8!"
+  )
+
+  val pinNames = pins
+
+  override def getTCL(moduleName: String, clock: String): String = {
+    var tcl = ""
+    for (pin <- 0 until pinNames.length) {
+      tcl += f"make_bd_pins_external  [get_bd_pins ${moduleName}/io_${this.getPartialName()}_${pin}]\n"
+      tcl += f"set_property NAME io_${this.getPartialName()}_${pin} [get_bd_ports /io_${this.getPartialName()}_${pin}_0]\n"
+    }
+    return tcl
+  }
 
   def getConstraints(): String = {
     var constraints = ""
-    for (pin <- 0 until this.port.get.amount) {
-      constraints += f"set_property PACKAGE_PIN ${names(pin)} [get_ports {io_${this.port.get.getPartialName()}_pins_${pin}}]\n"
-      constraints += f"set_property IOSTANDARD LVCMOS33 [get_ports {io_${this.port.get.getPartialName()}_pins_${pin}}]\n"
+    for ((name, pin) <- pinNames.zipWithIndex) {
+      constraints += f"set_property PACKAGE_PIN ${name} [get_ports {io_${this.getPartialName()}_${pin}}]\n"
+      constraints += f"set_property IOSTANDARD LVCMOS33 [get_ports {io_${this.getPartialName()}_${pin}}]\n"
     }
     return constraints
-  }
-
-  def init(port: PMOD): Unit = {
-    this.port = Some(port)
   }
 
 }
