9103901b8f9d17ec22fe34f8af70458601513c2c
Denis Hoornaert <denis.hoornaert@tum.de>
2025-06-25 00:00:05 +0200

    hardware: interface: gt and ethernet ip config: refactoring

--- hw/spinal/zcu102/io/ethernet.scala
+++ hw/spinal/zcu102/io/ethernet.scala
@@ -1,10 +1,11 @@
 package zcu102.io.ethernet
 
 
-import ultrascaleplus.io.ethernet._
+import ultrascaleplus.ip.{Ethernet}
+import ultrascaleplus.io.gt._
 
 
-object GT0 extends GTInstanceTemplate {
+object GT0 extends GTMappedTemplate() {
 
   /**
    * Note: The position of the pin names in the list matters! The position is the number of the pin in the pmod interface.
@@ -21,8 +22,9 @@ object GT0 extends GTInstanceTemplate {
    */
   override val RX = Seq[String]("B2", "B1")
 
-  override val group = "Quad_X1Y3"
-
-  override val lane = "X1Y14"
-
 }
+
+object Ethernet0 extends Ethernet.Config(
+  group = "Quad_X1Y3",
+  lane  = "X1Y14"
+)
