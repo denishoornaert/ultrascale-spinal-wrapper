0178fd74b8e4f1da3de0ab2b76b72f2e0ab834ed
Denis Hoornaert <denis.hoornaert@tum.de>
2025-07-24 19:03:59 +0200

    hardware: clock: introduce differential clock definition

--- hw/spinal/ultrascaleplus/clock/Clock.scala
+++ hw/spinal/ultrascaleplus/clock/Clock.scala
@@ -6,7 +6,7 @@ import spinal.lib._
 
 
 import ultrascaleplus.clock.pll.PllSource
-import ultrascaleplus.utils.{TCL, PSPLInterface, Log, Util}
+import ultrascaleplus.utils.{TCL, XDC, PSPLInterface, Log, Util}
 import ultrascaleplus.scripts.{TCLFactory}
 
 
@@ -48,6 +48,47 @@ class ClockMapped(val source: PllSource, val target: HertzNumber) extends Bundle
 
 } 
 
+class DiffClockMapped(val frequency: HertzNumber, val pin: String) extends Bundle with TCL with XDC with PSPLInterface {
+
+  val clock = new Bundle {
+    val p = in(Bool())
+    val n = in(Bool())
+  }
+
+  val domain = ClockDomain(
+    clock     = this.clock.p,
+    frequency = FixedFrequency(this.frequency), 
+  )
+
+  private val xilinxInterfaceName = "xilinx.com:interface:diff_clock_rtl:1.0"
+
+  override def getTCL(): String = {
+    var tcl = ""
+    tcl += f"set ${this.clock.getName()}_external [ create_bd_intf_port -mode Slave -vlnv ${this.xilinxInterfaceName} ${this.clock.getName()}_external ]"
+    tcl +=  "set_property -dict [ list \\"
+    tcl += f"  CONFIG.FREQ_HZ {${this.frequency.toBigDecimal}} \\"
+    tcl +=  "] $"+f"${this.clock.getName()}_external"
+    tcl +=  "\n"
+    tcl += TCLFactory.interfaceConnection(f"${this.clock.getName()}_intermediate", Seq(this.clock.getName(), f"${this.clock.getName()}_external"))
+    tcl +=  "\n"
+    return tcl
+  }
+
+  override def getXDC(): String = {
+    var constraint = ""
+    constraint += f"set_property PACKAGE_PIN ${this.pin} [get_ports ${this.clock.p.getName()}_external_clk_p]"
+    constraint += f"create_clock -period ${this.frequency.toTime.toBigDecimal} -name ${this.clock.getName()}_external [get_ports ${this.clock.getName()}_external_clk_p]"
+    constraint +=  "\n"
+    return constraint
+  }
+
+  override def setAttribute(): Unit = {
+    this.clock.p.addAttribute("X_INTERFACE_INFO", f"${this.xilinxInterfaceName} ${this.clock.n.getName()} CLK_P")
+    this.clock.n.addAttribute("X_INTERFACE_INFO", f"${this.xilinxInterfaceName} ${this.clock.p.getName()} CLK_N")
+  }
+
+}
+
 
 object ClockResetMapped {
 
@@ -62,7 +103,7 @@ object ClockResetMapped {
  *  @parameter source PLL clock source.
  *  @parameter target Target frequency desired.
  */
-class ClockResetMapped(source: PllSource, target: HertzNumber) extends ClockMapped(source: PllSource, target: HertzNumber) with TCL {
+class ClockResetMapped(source: PllSource, target: HertzNumber) extends ClockMapped(source: PllSource, target: HertzNumber) {
   
   val reset = in(Bool())
 
