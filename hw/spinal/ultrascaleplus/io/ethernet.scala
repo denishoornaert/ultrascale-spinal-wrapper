dcb09af02e24f1c24b9c359d41e011cad992a23c
Denis Hoornaert <denis.hoornaert@tum.de>
2025-08-08 12:03:59 +0200

    hardware: interface: gt: switch to diffbool type

--- hw/spinal/ultrascaleplus/io/ethernet.scala
+++ hw/spinal/ultrascaleplus/io/ethernet.scala
@@ -1,8 +1,11 @@
 package ultrascaleplus.io.gt
 
+
 import spinal.core._
 import spinal.lib._
 
+
+import ultrascaleplus.types._
 import ultrascaleplus.utils._
 
 
@@ -30,14 +33,8 @@ class GT() extends Bundle with IMasterSlave {
     in(this.rx)
   }
 
-  val tx = new Bundle {
-    val p = Bool()
-    val n = Bool()
-  }
-  val rx = new Bundle {
-    val p = Bool()
-    val n = Bool()
-  }
+  val tx = DiffBool()
+  val rx = DiffBool()
   val sfp = new Bundle {
     val dis = Bool()
   }
@@ -45,34 +42,38 @@ class GT() extends Bundle with IMasterSlave {
 }
 
 
-case class GTMapped(config: GTMappedTemplate) extends GT with PSPLInterface with TCL with XDC {
-  
-  assert(
-    assertion = (this.config.TX.length == 2),
-    message   = f"2 pins are expected for TX but ${this.config.TX.length} are provided!"
-  )
-    
-  assert(
-    assertion = (this.config.RX.length == 2),
-    message   = f"2 pins are expected for RX but ${this.config.RX.length} are provided!"
-  )
-  
+case class GTMapped(config: GTMappedTemplate) extends GT() with PSPLInterface with TCL with XDC {
+ 
   assert(
     assertion = (this.config.SFP.length == 1),
     message   = f"1 pins are expected for SFP but ${this.config.SFP.length} are provided!"
   )
   
+  assert(
+    assertion = (this.config.TX.length == 2),
+    message   = f"2 pins are expected for TX but ${this.config.TX.length} are provided!"
+  )
+  
+  assert(
+    assertion = (this.config.RX.length == 2),
+    message   = f"2 pins are expected for TX but ${this.config.RX.length} are provided!"
+  )
+    
   // TODO: could be changed for xilinx interface
-  override def getTCL(moduleName: String, clock: String): String = {
+  override def getTCL(): String = {
+    val moduleName = Util.topmodule(this).getName()
     var tcl = ""
+    // TX
     tcl += f"make_bd_pins_external  [get_bd_pins ${moduleName}/${this.tx.p.getName()}]\n"
     tcl += f"set_property NAME ${this.tx.p.getName()} [get_bd_ports /${this.tx.p.getName()}]\n"
     tcl += f"make_bd_pins_external  [get_bd_pins ${moduleName}/${this.tx.n.getName()}]\n"
     tcl += f"set_property NAME ${this.tx.n.getName()} [get_bd_ports /${this.tx.n.getName()}]\n"
+    // RX
     tcl += f"make_bd_pins_external  [get_bd_pins ${moduleName}/${this.rx.p.getName()}]\n"
     tcl += f"set_property NAME ${this.rx.p.getName()} [get_bd_ports /${this.rx.p.getName()}]\n"
     tcl += f"make_bd_pins_external  [get_bd_pins ${moduleName}/${this.rx.n.getName()}]\n"
     tcl += f"set_property NAME ${this.rx.n.getName()} [get_bd_ports /${this.rx.n.getName()}]\n"
+    // SFP DIS
     tcl += f"make_bd_pins_external  [get_bd_pins ${moduleName}/${this.sfp.dis.getName()}]\n"
     tcl += f"set_property NAME ${this.sfp.dis.getName()} [get_bd_ports /${this.sfp.dis.getName()}]\n"
     tcl += "\n"
@@ -81,20 +82,25 @@ case class GTMapped(config: GTMappedTemplate) extends GT with PSPLInterface with
     
   override def getXDC(): String = {
     var constraints = ""
+    // TX
     constraints += f"set_property PACKAGE_PIN ${this.config.TX(0)} [get_ports ${this.tx.p.getName()}]\n"
     constraints += f"set_property PACKAGE_PIN ${this.config.TX(1)} [get_ports ${this.tx.n.getName()}]\n"
-    constraints += f"set_property PACKAGE_PIN ${this.config.RX(0)} [get_ports ${this.tx.p.getName()}]\n"
-    constraints += f"set_property PACKAGE_PIN ${this.config.RX(1)} [get_ports ${this.tx.n.getName()}]\n"
+    // RX
+    constraints += f"set_property PACKAGE_PIN ${this.config.RX(0)} [get_ports ${this.rx.p.getName()}]\n"
+    constraints += f"set_property PACKAGE_PIN ${this.config.RX(1)} [get_ports ${this.rx.n.getName()}]\n"
+    // SFP DIS
     constraints += f"set_property PACKAGE_PIN ${this.config.SFP(0)} [get_ports ${this.sfp.dis.getName()}]\n"
     constraints += f"set_property IOSTANDARD LVCMOS33 [get_ports ${this.sfp.dis.getName()}]\n"
     return constraints
   }
 
   override def setAttribute(): Unit = {
-    this.tx.p.addAttribute("X_INTERFACE_INFO", f"xilinx.com:interface:sgmii:1.0 ${this.getPartialName()} TXP")
-    this.tx.n.addAttribute("X_INTERFACE_INFO", f"xilinx.com;interface:sgmii:1.0 ${this.getPartialName()} TXN")
-    this.rx.p.addAttribute("X_INTERFACE_INFO", f"xilinx.com:interface:sgmii:1.0 ${this.getPartialName()} RXP")
-    this.rx.n.addAttribute("X_INTERFACE_INFO", f"xilinx.com:interface:sgmii:1.0 ${this.getPartialName()} RXN")
+    // TX
+    this.tx.p.addAttribute("X_INTERFACE_INFO", f"xilinx.com:interface:sgmii:1.0 ${this.tx.getPartialName()} TXP")
+    this.tx.n.addAttribute("X_INTERFACE_INFO", f"xilinx.com;interface:sgmii:1.0 ${this.tx.getPartialName()} TXN")
+    // RX
+    this.rx.p.addAttribute("X_INTERFACE_INFO", f"xilinx.com:interface:sgmii:1.0 ${this.rx.getPartialName()} RXP")
+    this.rx.n.addAttribute("X_INTERFACE_INFO", f"xilinx.com;interface:sgmii:1.0 ${this.rx.getPartialName()} RXN")
   }
 
 }
