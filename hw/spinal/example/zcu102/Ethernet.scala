cdf5cd71117f22e8d82c2288d206afd3dc8429ab
Denis Hoornaert <denis.hoornaert@tum.de>
2025-09-07 14:33:06 +0200

    example: zcu102: ethernet: connect hpm to axi-lite configuration port

--- hw/spinal/example/zcu102/Ethernet.scala
+++ hw/spinal/example/zcu102/Ethernet.scala
@@ -3,6 +3,9 @@ package example.zcu102
 
 import spinal.core._
 import spinal.lib._
+import spinal.lib.bus.amba4.axi.Axi4
+import spinal.lib.bus.amba4.axilite.AxiLite4
+import spinal.lib.bus.amba4.axilite.AxiLite4Utils.Axi4Rich
 
 
 import ultrascaleplus.clock.PLClockingArea
@@ -16,15 +19,22 @@ import zcu102.io.ethernet._
 case class EthernetBB() extends ZCU102(
   config    = new ZCU102Config(
     withPL_CLK0   = 332.00 MHz,
+    withLPD_HPM0  =       true,
     with_GT0      =       true,
     withSI570_MGT = 156.25 MHz
   )
 ) {
 
+  io.lpd.hpm0.associate(io.pl.clk0)
+
+  val ethernetOutputClock = Bool()
+  
   val plclk0 = new PLClockingArea(io.pl.clk0) {
     val ctrl = Ethernet(Ethernet0)
+    ctrl.io.axi <> Axi4Rich(io.lpd.hpm0).toLite(ctrl.io.axi.config)
     ctrl.io.refclk <> io.user.si570.mgt
     ctrl.io.gt <> io.gt0
+    ethernetOutputClock := ctrl.io.tx.clk.o
   }
 
   this.generate()
