4d5e7be17c161142bb7a946bdef061f997e5192d
Denis Hoornaert <denis.hoornaert@tum.de>
2025-09-07 15:05:44 +0200

    merge fix

--- hw/spinal/ultrascaleplus/io/ethernet.scala
+++ hw/spinal/ultrascaleplus/io/ethernet.scala
@@ -2,29 +2,27 @@ package ultrascaleplus.io.ethernet
 
 import spinal.core._
 import spinal.lib._
-import spinal.lib.bus.amba4.axi._
 
 import ultrascaleplus.utils._
 
-/** SFP interface bundle/interface class.
+/** GT interface bundle/interface class.
  *
  *  NOTE: extending [[TCL]] and [[XDC]] is not needed as the component do.
  *
  *  @constructor Creates an SFP interface.
  *  @param txPins TX board pins' name for the platform.
+ *  @param sfpPins SFP board pins' name for the platform.
  *  @param rxPins RX board pins' name for the platform.
  */
-case class SFP(txPins: Seq[String], rxPins: Seq[String]) extends Bundle {
+case class GT(txPins: Seq[String], sfpPins: Seq[String], rxPins: Seq[String]) extends Bundle {
   
-  case class TX(pins: Seq[String]) extends Bundle with TCL with XDC {
+  case class SFP(pins: Seq[String]) extends Bundle with TCL with XDC {
     
     assert(
-      assertion = (pins.length == 3),
-      message   = f"3 pins are expected but ${pins.length} are provided!"
+      assertion = (pins.length == 1),
+      message   = f"1 pins are expected but ${pins.length} are provided!"
     )
 
-    val p   = Bool()
-    val n   = Bool()
     val dis = Bool()
 
     override def getTCL(moduleName: String, clock: String): String = {
@@ -37,23 +35,21 @@ case class SFP(txPins: Seq[String], rxPins: Seq[String]) extends Bundle {
 
     override def getXDC(): String = {
       var constraints = ""
-      constraints += f"set_property PACKAGE_PIN ${pins(0)} [get_ports ${this.p.getName()}]\n"
-      constraints += f"set_property PACKAGE_PIN ${pins(1)} [get_ports ${this.n.getName()}]\n"
-      constraints += f"set_property PACKAGE_PIN ${pins(2)} [get_ports ${this.dis.getName()}]\n"
+      constraints += f"set_property PACKAGE_PIN ${pins(0)} [get_ports ${this.dis.getName()}]\n"
       constraints += f"set_property IOSTANDARD LVCMOS33 [get_ports ${this.dis.getName()}]\n"
       return constraints
     }
   }
 
-  case class RX(pins: Seq[String]) extends Bundle with TCL with XDC {
+  case class DifferentialPaire(pins: Seq[String]) extends Bundle with TCL with XDC {
 
     assert(
       assertion = (pins.length == 2),
       message   = f"2 pins are expected but ${pins.length} are provided!"
     )
     
-    val p   = Bool()
-    val n   = Bool()
+    val p = Bool()
+    val n = Bool()
 
     override def getTCL(moduleName: String, clock: String): String = {
       var tcl = ""
@@ -71,7 +67,8 @@ case class SFP(txPins: Seq[String], rxPins: Seq[String]) extends Bundle {
     }
   }
 
-  val tx = out(TX(txPins))
-  val rx =  in(RX(rxPins))
+  val tx  = out(DifferentialPaire(txPins))
+  val sfp = out(SFP(sfpPins))
+  val rx  =  in(DifferentialPaire(rxPins))
 
 }
--- hw/spinal/zcu102/io/ethernet.scala
+++ hw/spinal/zcu102/io/ethernet.scala
@@ -4,12 +4,17 @@ package zcu102.io.ethernet
 import ultrascaleplus.io.ethernet._
 
 
-object SFP0 {
+object GT0 {
 
   /**
    * Note: The position of the pin names in the list matters! The position is the number of the pin in the pmod interface.
    */
-  val TX = Seq[String]("B6", "B5", "B13")
+  val TX = Seq[String]("B6", "B5")
+
+  /**``
+   * Note: The position of the pin names in the list matters! The position is the number of the pin in the pmod interface.
+   */
+  val SFP = Seq[String]("B13")
 
   /**
    * Note: The position of the pin names in the list matters! The position is the number of the pin in the pmod interface.
