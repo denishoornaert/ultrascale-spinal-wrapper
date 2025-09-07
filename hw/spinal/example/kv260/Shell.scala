9a31955f537230e96aff4c70af7516e9fa566a83
Denis Hoornaert <denis.hoornaert@tum.de>
2025-09-07 14:31:13 +0200

    example: kv260: shell: shuffle clock assignments

--- hw/spinal/example/kv260/Shell.scala
+++ hw/spinal/example/kv260/Shell.scala
@@ -13,10 +13,10 @@ import kv260._
 
 case class Shell() extends KV260(
   config    = new KV260Config(
-    withPL_CLK0    = 332 MHz,
-    withPL_CLK1    = 332 MHz,
-    withPL_CLK2    = 332 MHz,
-    withPL_CLK3    = 332 MHz,
+    withPL_CLK0    = 250 MHz,
+    withPL_CLK1    = 250 MHz,
+    withPL_CLK2    = 250 MHz,
+    withPL_CLK3    = 250 MHz,
     withLPD_HPM0   =    true,
     withLPD_HP0    =    true,
     withFPD_HPM0   =    true,
@@ -48,14 +48,16 @@ case class Shell() extends KV260(
   io.lpd.hp0.associate(io.pl.clk0)
   io.lpd.hpm0.associate(io.pl.clk0)
   io.fpd.hpm0.associate(io.pl.clk0)
-  io.fpd.hpm1.associate(io.pl.clk0)
+  io.fpd.hpm1.associate(io.pl.clk1)
   io.fpd.hp0.associate(io.pl.clk0)
-  io.fpd.hp1.associate(io.pl.clk0)
-  io.fpd.hp2.associate(io.pl.clk0)
-  io.fpd.hp3.associate(io.pl.clk0)
+  io.fpd.hp1.associate(io.pl.clk1)
+  io.fpd.hp2.associate(io.pl.clk2)
+  io.fpd.hp3.associate(io.pl.clk3)
   io.fpd.hpc0.associate(io.pl.clk0)
-  io.fpd.hpc1.associate(io.pl.clk0)
-  io.fpd.acp.associate(io.pl.clk0)
+  io.fpd.hpc1.associate(io.pl.clk1)
+  io.fpd.acp.associate(io.pl.clk2)
+
+  io.trace.clk.associate(io.pl.clk0)
 
   val plclk0 = new PLClockingArea(io.pl.clk0) {
     // Create dummy register to force instantiation of cock and reset I/O
@@ -76,6 +78,12 @@ case class Shell() extends KV260(
   }
 
   val plclk3 = new PLClockingArea(io.pl.clk3) {
+    // Create dummy register to force instantiation of cock and reset I/O
+    val clock_count = Reg(UInt(64 bits)) init(0)
+    clock_count := clock_count+1
+  }
+
+  val traceclk = new PLClockingArea(io.trace.clk) {
     // Create dummy register to force instantiation of cock and reset I/O
     val clock_count = Reg(UInt(64 bits)) init(0)
     clock_count := clock_count+1
