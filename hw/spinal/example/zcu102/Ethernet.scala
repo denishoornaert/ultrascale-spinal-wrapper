5b784ffee67e71e21a8cad57c3eedec33fe0acd4
Denis Hoornaert <denis.hoornaert@tum.de>
2025-09-10 18:50:55 +0200

    hardware; example; zcu102: ethernet: change clock target

--- hw/spinal/example/zcu102/Ethernet.scala
+++ hw/spinal/example/zcu102/Ethernet.scala
@@ -19,7 +19,7 @@ import zcu102.io.ethernet._
 
 case class EthernetBB() extends ZCU102(
   config    = new ZCU102Config(
-    withPL_CLK0   = 332.00 MHz,
+    withPL_CLK0   =  75.00 MHz,
     withLPD_HPM0  =       true,
     with_GT0      =       true,
     withSI570_MGT = 156.25 MHz
@@ -28,14 +28,11 @@ case class EthernetBB() extends ZCU102(
 
   io.lpd.hpm0.associate(io.pl.clk0)
 
-  val ethernetOutputClock = Bool()
-  
   val plclk0 = new PLClockingArea(io.pl.clk0) {
     val ctrl = Ethernet(Ethernet0)
     ctrl.io.axi <> Axi4Rich(io.lpd.hpm0).toLite(ctrl.io.axi.config)
     ctrl.io.refclk <> io.user.si570.mgt
     ctrl.io.gt <> io.gt0
-    ethernetOutputClock := ctrl.io.tx.clk.o
   }
 
   this.generate()
