d156f4d9531b594cf4aef4f1d192a9c0ceafff74
Denis Hoornaert <denis.hoornaert@tum.de>
2025-09-25 13:45:23 +0200

    hardware: clock: pll: switch computation to more efficient logic inspired from Xilinx linux kernel module and firmware

--- hw/spinal/ultrascaleplus/clock/Pll.scala
+++ hw/spinal/ultrascaleplus/clock/Pll.scala
@@ -1,6 +1,9 @@
 package ultrascaleplus.clock.pll
 
 
+import scala.math.{floor, ceil, round, pow}
+
+
 import spinal.core._
 import spinal.lib._
 
@@ -19,44 +22,59 @@ case class PLL(name: String, frequency: HertzNumber) {
 
 object PLL {
 
-  case class Config(m: Int, d0: Int, d1: Int, d2: Int, fvco: HertzNumber, fout: HertzNumber) {}
+  case class Config(m: Int, d0: Int, d1: Int, d2: Int, fvco: HertzNumber) {
+
+    val fout: HertzNumber = fvco/(2*d0*d1*d2)
+
+  }
 
   case class Ranges(m: Range, d0: Range, d1: Range) {}
 
   private def quantize(frequency: HertzNumber, scale: Int = 6): HertzNumber = {
     return HertzNumber(frequency.toBigDecimal.setScale(scale, BigDecimal.RoundingMode.HALF_UP))
   }
+
+  private def clamp(value: Int, range: Range): Int = {
+    return (value max range.min) min range.max
+  }
  
   /**
-   * Branch and bound. Looking for best match
+   * Branch and bound. Looking for best match.
+   *
+   * Mix of logic found from xilinx sources:
+   *  - https://github.com/Xilinx/linux-xlnx/blob/786191c466220b9fe979f81053b82cb095d18225/drivers/clk/zynqmp/pll.c#L101
+   *  - https://github.com/Xilinx/embeddedsw/blob/45a18907084e77bb3a450a035d280130d7ff6e26/lib/sw_services/xilpm/src/zynqmp/client/common/pm_clock.c#L1107
    */
   def tune(fin: HertzNumber, target: HertzNumber, ranges: Ranges): PLL.Config = {
-    var bestConfig: PLL.Config  = null
-    var bestError : HertzNumber = HertzNumber(Double.MinValue)
-
-    for {
-      d1 <- ranges.d1
-      d0 <- ranges.d0
-      m  <- ranges.m
-    } {
-      val fvco = fin*m
-      // Detect if div2 is needed
-      val div2 = if((fvco/target) > ranges.d0.max*ranges.d1.max) Seq(1, 2, 4, 8) else Seq(1)
-      // Search
-      for (d2 <- div2) {
-        // Reportedly, there is a fixed /2 factor
-        val fout = PLL.quantize(fvco/(2*d0*d1*d2))
-        // Note error must be the smallest non strictly positive frequency
-        val error = fout-target
-        if ((error <= HertzNumber(0)) && (bestError < error)) {
-          bestError = error
-          bestConfig = PLL.Config(m, d0, d1, d2, fvco, fout)
-          println(f"${error} -> Config(${m}, ${d0}, ${d1}, ${d2}, ${fvco}, ${fout})")
-        }
-      }
+    val divRange = (0 to 63)
+    var config: Option[PLL.Config] = None
+    val m = 90
+    val fvco = fin.toDouble*m
+    val div = ceil(fvco/(2*target.toDouble)).toInt
+    // Breakdown overall divisor in two divisors
+    assert(
+      assertion = div < pow(divRange.max, 2),
+      message   = f"Rate (${fvco}) must be smaller than ${pow(2, divRange.max)}!"
+    )
+    // Fits in first divisor -> straight forward mapping
+    if (divRange contains div) {
+      config = Some(PLL.Config(m, div.toInt, 1, 1, HertzNumber(fvco)))
     }
-
-    return bestConfig
+    // Must be decomposed
+    else {
+      val decomposition = (2 to ceil(divRange.max/2).toInt).filter(d0 => fvco%d0 == 0).map(d0 => (d0, floor(fvco.toLong/d0).toInt))
+      assert(
+        assertion = decomposition(0)._2 != 0,
+        message   = "Divisor is a prime number bigger than the encoding width."
+      )
+      config = Some(PLL.Config(m, decomposition(0)._1, decomposition(0)._2, 1, HertzNumber(fvco)))
+    }
+    assert(
+      assertion = config.isDefined,
+      message   = f"No PLL configuration could not be found for target ${target}."
+    )
+    println(f"Config(${config.get.m}, ${config.get.d0}, ${config.get.d1}, ${config.get.d2}, ${config.get.fvco}, ${config.get.fout})")
+    return config.get
   }
 
   object IO {
