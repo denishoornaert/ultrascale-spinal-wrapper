816ee75e52ad75e4e8531d55d9cacc5b0a5611fb
Denis Hoornaert <denis.hoornaert@tum.de>
2025-10-02 10:47:07 +0200

    hardware: example: change clock settings

--- hw/spinal/example/kr260/Shell.scala
+++ hw/spinal/example/kr260/Shell.scala
@@ -15,10 +15,10 @@ import kr260._
 
 case class Shell() extends KR260(
   config    = new KR260Config(
-    withPL_CLK0    = PLL.IO(332 MHz),
+    withPL_CLK0    = PLL.IO(250 MHz),
     withPL_CLK1    = PLL.IO(332 MHz),
     withPL_CLK2    = PLL.IO(332 MHz),
-    withPL_CLK3    = PLL.IO(332 MHz),
+    withPL_CLK3    = PLL.R( 75 MHz),
     withLPD_HPM0   =    true,
     withLPD_HP0    =    true,
     withFPD_HPM0   =    true,
@@ -53,14 +53,16 @@ case class Shell() extends KR260(
   io.lpd.hp0.associate(io.pl.clk0)
   io.lpd.hpm0.associate(io.pl.clk0)
   io.fpd.hpm0.associate(io.pl.clk0)
-  io.fpd.hpm1.associate(io.pl.clk0)
+  io.fpd.hpm1.associate(io.pl.clk1)
   io.fpd.hp0.associate(io.pl.clk0)
-  io.fpd.hp1.associate(io.pl.clk0)
-  io.fpd.hp2.associate(io.pl.clk0)
-  io.fpd.hp3.associate(io.pl.clk0)
+  io.fpd.hp1.associate(io.pl.clk1)
+  io.fpd.hp2.associate(io.pl.clk2)
+  io.fpd.hp3.associate(io.pl.clk3)
   io.fpd.hpc0.associate(io.pl.clk0)
-  io.fpd.hpc1.associate(io.pl.clk0)
-  io.fpd.acp.associate(io.pl.clk0)
+  io.fpd.hpc1.associate(io.pl.clk1)
+  io.fpd.acp.associate(io.pl.clk2)
+  
+  io.trace.clk.associate(io.pl.clk0)
 
   val plclk0 = new PLClockingArea(io.pl.clk0) {
     // Create dummy register to force instantiation of cock and reset I/O
--- hw/spinal/example/kv260/Shell.scala
+++ hw/spinal/example/kv260/Shell.scala
@@ -15,9 +15,9 @@ import kv260._
 
 case class Shell() extends KV260(
   config    = new KV260Config(
-    withPL_CLK0    = PLL.IO(340 MHz),
-    withPL_CLK1    = PLL.IO(300 MHz),
-    withPL_CLK2    = PLL.IO(250 MHz),
+    withPL_CLK0    = PLL.IO(250 MHz),
+    withPL_CLK1    = PLL.IO(2500 MHz),
+    withPL_CLK2    = PLL.IO(334 MHz),
     withPL_CLK3    = PLL.R( 75 MHz),
     withLPD_HPM0   =    true,
     withLPD_HP0    =    true,
